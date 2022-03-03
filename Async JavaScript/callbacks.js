9f9c1d16c4aeb94776185fda0a56d39a9e4eeaf7
Shay R <shalondra.davis@protonmail.com>
2022-03-02 19:45:42 -0800

    Create new files for the project that include basic HTML and JS functions.

---
+++ Async JavaScript/callbacks.js
@@ -0,0 +1,13 @@
+// Here, we are going to mimick blog posts on a server, getting them and then creating a blog post.
+
+// First we'll create an array called 'posts' with teo objects:
+const posts = [
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
+];
+
+// Remember, we are mimicking a request to fetch info from a server, and that could take a couple seconds
+function getPosts() {
+    //The 'setTimeout()' method takes in a callback function as well as a number of milliseconds
+    setTimeout();
+}
