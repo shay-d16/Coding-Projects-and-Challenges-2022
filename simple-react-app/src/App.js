dab9a4c6f98959f3a4d4d7f03d235557cf271efb
Shay R <shalondra.davis@protonmail.com>
2022-03-09 20:59:25 -0800

    Finish remaining notes, fix useEffect (1)

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -21,15 +21,29 @@ function App() {
 
   // We need to make sure the todos are persisted across page reload, and we can do this by storing the todos in local storage
   // using the 'useEffect' hook, which is a function that takes
-  // 
- useEffect(() => {
-   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-   setTodos(storedTodos)
- }, [])
+  useEffect(() => {
+    // In this function will be called to load the todos
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) //use 'JSON.parse' to convert the string to an array
+    //Here we set the value to what we get back from our stored todos in the 'storedTodos' variable, which is then passed
+    // to the 'setTodos' function
+    if (storedTodos) setTodos(storedTodos) //we set our todos to that 'storedTodos' only if we have stored todos
+  }, []) 
+  // We will only want to call this function once right when the component loads, so if we pass in an empty array of
+  // dependancies, it will call this function once. Since the empty array never changes, it will never re-call this 
+  // 'useEffect' function.
 
   useEffect(() => {
+  // This takes a function as it's first parameter, and it will be called every time something changes. The way we determine 
+  // when to call that function is by passing in an array of properties here.
+  // This array is going to be all of our dependancies.
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
+  // So any time our array of todos changes, it will run this 'useEffect' function and save the todos in the variable above
+  // called 'LOCAL_STORAGE_KEY'. We also used ''JSON.stringify' to make sure the todo is passed as a string.
+  
+  // At this point, the todos are saved to the local storage, but still don't show up when the page refreshes. This is 
+  // because we'll need to have another 'useEffect', located above this function.
+
 
   // We now need a way to toggle todos, because even if we click the checkbox, it's not actually being checked. Using React,
   // we're going to store the change of our todo from being not complete to complete and vice-versa.
@@ -42,8 +56,15 @@ function App() {
     
     // Now, we'll get the todo that we're actually trying to modify:
     const todo = newTodos.find(todo => todo.id === id)
+    // We can get this new todo by finding the todo that has a 'todo.id' equal to the id that was passed into this 
+    // 'toggleTodo' function.
     todo.complete = !todo.complete
+    // Here, we are setting 'todo.complete' equal to the opposite of 'todo.complete', then we can set the todos to
+    // 'newTodos'
     setTodos(newTodos)
+    // This function allows us toggle between complete and incomplete (checked and unchecked).
+    // To use this function, we have to pass it down to our 'TodoList' as a prop, then import the prop to the 
+    // 'TodoList.js' and 'Todo.js' components and pass it down to individual todos.
   }
 
   function handleAddTodo(e) { //pass in the 'event' property 'e'
@@ -70,8 +91,10 @@ function App() {
   }
 
   function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
+    // This function will set our todos to the new list that doesn't have any of our completed todos
+    const newTodos = todos.filter(todo => !todo.complete) //the value is filtered to all the todos that are not complete.
     setTodos(newTodos)
+    // Now when you refresh, it only shows the incomplete todos on the list.
   }
 
   // You can use parentheses to 'return' anything within them
@@ -89,11 +112,17 @@ function App() {
     </>
     // The first element of this app is a JSX element that imports the 'TodoList()' function from the 'TodoList.js' file
     // and returns it here. We also passed in the props (properties) just like we would attributes to an HTML element. In this
-    // case, the 'todos={todos}' is saying we have a prop 'todos' in our 'TodoList', and we want to pass that 'todos' variable 
-    // to that prop 'todos'.
+    // case, the 'todos={todos}' is saying we have a prop 'todos' in our 'TodoList', and we want to pass the value of the 'todos' 
+    // variable to the prop 'todos', as well as the 'toggleTodo' prop.
+    
     // The second element is the input for adding a todo with a 'ref' prop set to 'todoNameRef'
+    
     // The third is a button for creating todos with an 'onClick' prop set to the 'handleAddTodo' funtion.
-    // The fourth is a button for clearing the todo, and lastly the 'div' that says how many todos are left to do.
+    
+    // The fourth is a button for clearing the todo by using an 'onClick' event listener to call the 'handleClearTodos' function.
+    
+    // The fifth is a 'div' that shows how many todos are left to do using a 'filter' function to filter all the todos that are
+    // not checked and print the number of todos that are not complete.
     
     
   )
--- simple-react-app/src/Todo.js
+++ simple-react-app/src/Todo.js
@@ -3,14 +3,21 @@ import React from 'react'
 export default function Todo({ todo, toggleTodo }) { //pass in the props
   function handleTodoClick() {
     toggleTodo(todo.id)
+    // This function just calls the 'toggleTodo' function and passes in the 'todo.id'
   }
   return (
-    // This will be the checkbox for the todos. Here, we will set the 'checked' property and set the value to 'todo.complete'
+    // This will be the checkbox for the todos. Here, we will set the 'checked' property and set the value to 'todo.complete' and
+    // used an 'onChange' attribute to call the 'handleTodoClick' function
     <div>
         <label>
             <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
             {todo.name}
         </label>
     </div>
+    // Now when you check (or uncheck) the checkbox and refresh the page, you will see that React has stored the 'complete'
+    // status inside of our todos, which is inside of local storage. So, every time you click the checkbox it calls the
+    // 'handleTodoClick' function, which calls the 'toggleTodo' function with the id of the todo we are inside of; that, in turn,
+    // gets passed up through 'TodoList.js' into our 'App.js' and calls up it's 'toggleTodo' function, which resets our 'TodoList'
+    // variable to the new list of todos with the todo that we clicked on checked. 
   )
 }
